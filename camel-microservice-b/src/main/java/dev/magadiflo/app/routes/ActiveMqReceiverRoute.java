09de7fa5b27db10ee6118f61dba868fc1ddf3338
Martín <magadiflo@gmail.com>
2024-11-10 13:12:21 -0500

    Transformación y procesamiento después de la desagrupación (unmashalling) en Camel Route: Procesamiento

--- camel-microservice-b/src/main/java/dev/magadiflo/app/routes/ActiveMqReceiverRoute.java
+++ camel-microservice-b/src/main/java/dev/magadiflo/app/routes/ActiveMqReceiverRoute.java
@@ -1,17 +1,34 @@
 package dev.magadiflo.app.routes;
 
 import dev.magadiflo.app.models.CurrencyExchange;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.model.dataformat.JsonLibrary;
 import org.springframework.stereotype.Component;
 
+@Slf4j
+@RequiredArgsConstructor
 @Component
 public class ActiveMqReceiverRoute extends RouteBuilder {
+
+    private final MyCurrencyExchangeProcessor myCurrencyExchangeProcessor;
 
     @Override
     public void configure() throws Exception {
         from("activemq:my-activemq-queue")
                 .unmarshal().json(JsonLibrary.Jackson, CurrencyExchange.class)
+                .bean(this.myCurrencyExchangeProcessor)
                 .to("log:received-message-from-active-mq");
     }
+}
+
+@Slf4j
+@Component
+class MyCurrencyExchangeProcessor {
+
+    public void processMessage(CurrencyExchange currencyExchange) {
+        log.info("Realiza procesamiento con currencyExchange.getConversionMultiple() cuyo valor es {}", currencyExchange.getConversionMultiple());
+    }
+
 }
