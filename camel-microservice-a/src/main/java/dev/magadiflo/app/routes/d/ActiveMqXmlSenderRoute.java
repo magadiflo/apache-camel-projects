f3f8e60c0b0db00c8fb85d2e42e5acbce1d5ba50
Martín <magadiflo@gmail.com>
2024-11-10 19:40:43 -0500

    Desagrupación (unmarshalling) de mensajes XML en Java Bean en Camel Route de ActiveMQ

--- camel-microservice-a/src/main/java/dev/magadiflo/app/routes/d/ActiveMqXmlSenderRoute.java
+++ camel-microservice-a/src/main/java/dev/magadiflo/app/routes/d/ActiveMqXmlSenderRoute.java
@@ -1,14 +1,14 @@
-package dev.magadiflo.app.routes.c;
+package dev.magadiflo.app.routes.d;
 
 import org.apache.camel.builder.RouteBuilder;
 import org.springframework.stereotype.Component;
 
-//@Component
-public class ActiveMqSenderRoute extends RouteBuilder {
+@Component
+public class ActiveMqXmlSenderRoute extends RouteBuilder {
     @Override
     public void configure() throws Exception {
-        from("file:files/json")
+        from("file:files/xml")
                 .log("${body}")
-                .to("activemq:my-activemq-queue");
+                .to("activemq:my-activemq-xml-queue");
     }
 }
