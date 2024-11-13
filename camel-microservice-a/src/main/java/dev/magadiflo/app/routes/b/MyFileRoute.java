019bac503de3520b75bbe8a49bcb894a0f82ed7b
Martín <magadiflo@gmail.com>
2024-11-13 15:13:22 -0500

    Crea Decisores Complejos para Choice en Rutas de Camel

--- camel-microservice-a/src/main/java/dev/magadiflo/app/routes/b/MyFileRoute.java
+++ camel-microservice-a/src/main/java/dev/magadiflo/app/routes/b/MyFileRoute.java
@@ -1,10 +1,20 @@
 package dev.magadiflo.app.routes.b;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.camel.Body;
+import org.apache.camel.Headers;
 import org.apache.camel.builder.RouteBuilder;
 import org.springframework.stereotype.Component;
 
+import java.util.Map;
+
+@RequiredArgsConstructor
 @Component
 public class MyFileRoute extends RouteBuilder {
+
+    private final DeciderBean deciderBean;
+
     @Override
     public void configure() throws Exception {
         from("file:files/input")
@@ -14,20 +24,27 @@ public class MyFileRoute extends RouteBuilder {
                 .choice()
                 .when(simple("${file:ext} ends with 'xml'"))
                 .log("XML FILE")
-                .when(simple("${body} contains 'USD'"))
+                .when(method(this.deciderBean, "isThisConditionMethod2"))
                 .log("No es un archivo XML pero contiene USD")
                 .otherwise()
                 .log("No es un archivo XML")
                 .end()
                 //
-                .to("direct:log-file-values")
                 .to("file:files/output");
+    }
+}
 
-        from("direct:log-file-values")
-                .log("${messageHistory} ${file:absolute.path}")
-                .log("${file:name} ${file:name.ext} ${file:name.noext} ${file:onlyname}")
-                .log("${file:onlyname.noext} ${file:parent} ${file:path} ${file:absolute}")
-                .log("${file:size} ${file:modified}")
-                .log("${routeId} ${camelId} ${body}");
+@Slf4j
+@Component
+class DeciderBean {
+    public boolean isThisConditionMethod(String body) {
+        log.info("{}", body);
+        return true;
+    }
+
+    public boolean isThisConditionMethod2(@Body String body, @Headers Map<String, String> headers) {
+        log.info("{}", body);
+        log.info("{}", headers);
+        return true;
     }
 }
