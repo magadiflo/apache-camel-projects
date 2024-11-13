e8d64cf628e066ef646945941628e3acc9bcbc6c
Martín <magadiflo@gmail.com>
2024-11-12 20:31:25 -0500

    Usa choice y lenguaje simple en Rutas Camel: Nuevo when

--- camel-microservice-a/src/main/java/dev/magadiflo/app/routes/b/MyFileRoute.java
+++ camel-microservice-a/src/main/java/dev/magadiflo/app/routes/b/MyFileRoute.java
@@ -9,10 +9,13 @@ public class MyFileRoute extends RouteBuilder {
     public void configure() throws Exception {
         from("file:files/input")
                 .routeId("files-input-route-id")
+                .transform().body(String.class)
                 //
                 .choice()
                 .when(simple("${file:ext} ends with 'xml'"))
                 .log("XML FILE")
+                .when(simple("${body} contains 'USD'"))
+                .log("No es un archivo XML pero contiene USD")
                 .otherwise()
                 .log("No es un archivo XML")
                 .end()
