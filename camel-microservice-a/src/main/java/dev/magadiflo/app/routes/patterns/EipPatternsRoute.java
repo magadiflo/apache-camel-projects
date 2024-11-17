ec0d0f7d46a70e8d2d03d412ad4e4e820bfa9e3f
Martín <magadiflo@gmail.com>
2024-11-16 20:17:14 -0500

    Explora patrón de integración empresarial Splitter en Camel

--- camel-microservice-a/src/main/java/dev/magadiflo/app/routes/patterns/EipPatternsRoute.java
+++ camel-microservice-a/src/main/java/dev/magadiflo/app/routes/patterns/EipPatternsRoute.java
@@ -1,15 +1,32 @@
 package dev.magadiflo.app.routes.patterns;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.camel.builder.RouteBuilder;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+
+@RequiredArgsConstructor
 @Component
 public class EipPatternsRoute extends RouteBuilder {
+
+    private final SplitterComponent splitterComponent;
+
     @Override
     public void configure() throws Exception {
         from("file:files/csv")
-                .unmarshal().csv()
-                .split(body())
+                .convertBodyTo(String.class)
+                .split(method(this.splitterComponent))
                 .to("log:split-files");
+    }
+}
+
+@Slf4j
+@Component
+class SplitterComponent {
+    public List<String> splitInput(String body) {
+        log.info("body: {}", body);
+        return List.of("abc", "def", "ghi");
     }
 }
