57930c705848009c93a8e9d57dd85c408ea12551
Martín <magadiflo@gmail.com>
2024-11-17 17:10:25 -0500

    Explora patrón de integración empresarial Aggregation en Camel

--- camel-microservice-a/src/main/java/dev/magadiflo/app/routes/patterns/EipPatternsRoute.java
+++ camel-microservice-a/src/main/java/dev/magadiflo/app/routes/patterns/EipPatternsRoute.java
@@ -1,32 +1,49 @@
 package dev.magadiflo.app.routes.patterns;
 
+import dev.magadiflo.app.models.CurrencyExchange;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.camel.AggregationStrategy;
+import org.apache.camel.Exchange;
 import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.model.dataformat.JsonLibrary;
 import org.springframework.stereotype.Component;
 
-import java.util.List;
+import java.util.ArrayList;
 
 @RequiredArgsConstructor
 @Component
 public class EipPatternsRoute extends RouteBuilder {
 
-    private final SplitterComponent splitterComponent;
-
     @Override
     public void configure() throws Exception {
-        from("file:files/csv")
-                .convertBodyTo(String.class)
-                .split(method(this.splitterComponent))
-                .to("log:split-files");
+        from("file:files/aggregate-json")
+                .unmarshal().json(JsonLibrary.Jackson, CurrencyExchange.class)
+                .aggregate(simple("${body.to}"), new ArrayListAggregationStrategy())
+                .completionSize(3)
+                .to("log:aggregate-json");
     }
 }
 
+/**
+ * Estrategia de agregación de lista
+ */
 @Slf4j
-@Component
-class SplitterComponent {
-    public List<String> splitInput(String body) {
-        log.info("body: {}", body);
-        return List.of("abc", "def", "ghi");
+class ArrayListAggregationStrategy implements AggregationStrategy {
+    @Override
+    public Exchange aggregate(Exchange oldExchange, Exchange newExchange) {
+        Object newBody = newExchange.getIn().getBody();
+        ArrayList<Object> list = null;
+
+        if (oldExchange == null) {
+            list = new ArrayList<>();
+            list.add(newBody);
+            newExchange.getIn().setBody(list);
+            return newExchange;
+        } else {
+            list = oldExchange.getIn().getBody(ArrayList.class);
+            list.add(newBody);
+            return oldExchange;
+        }
     }
 }
